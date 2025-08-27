49dbb0648c47930a5037a0e3eed5473439ad4c9f
EmreHaksever <emre.haksever2@gmail.com>
2025-08-27 13:35:26 +0300

    Proje dosyası ekle.

---
+++ ToDo/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+
+        // Navigation property
+        public List<TaskItem> Tasks { get; set; }
+    }
+}
