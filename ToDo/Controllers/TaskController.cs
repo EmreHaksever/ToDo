04ccd41dd171e8d3eed7576da8a39c6c99486371
EmreHaksever <emre.haksever2@gmail.com>
2025-08-27 14:54:13 +0300

    TaskController has added and some api writed.

---
+++ ToDo/Controllers/TaskController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Data;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/task
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/task/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItem>> GetTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+            return task;
+        }
+
+        // POST: api/task
+        [HttpPost]
+        public async Task<ActionResult<TaskItem>> PostTask(TaskItem task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTask), new { id = task.Id }, task);
+        }
+
+        // PUT: api/task/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(int id, TaskItem task)
+        {
+            if (id != task.Id) return BadRequest();
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Tasks.Any(t => t.Id == id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/task/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null) return NotFound();
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
