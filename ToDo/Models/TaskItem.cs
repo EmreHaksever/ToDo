5156a61d20d543eb3c2c8ecd814df9d3cfcbc416
EmreHaksever <emre.haksever2@gmail.com>
2025-08-27 13:38:48 +0300

    TaskItem added.

--- ToDo/Models/TaskItem.cs
+++ ToDo/Models/TaskItem.cs
@@ -2,5 +2,14 @@
 {
     public class TaskItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime? DueDate { get; set; }
+
+        // Foreign Key
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
